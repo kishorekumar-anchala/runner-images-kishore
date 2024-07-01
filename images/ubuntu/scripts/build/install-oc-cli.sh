b4ce6f9c7eb8e2d0b0f98dff140d27fdac623a8d
Kishore Kumar Reddy Anchala <kishorekumar-anchala@github.com>
2024-07-01 18:00:50 +0530

    troublshooting openshift issue on ubuntu20

--- images/ubuntu/scripts/build/install-oc-cli.sh
+++ images/ubuntu/scripts/build/install-oc-cli.sh
@@ -5,16 +5,12 @@
 ################################################################################
 
 # Source the helpers for use with the script
-source $HELPER_SCRIPTS/os.sh
+
 source $HELPER_SCRIPTS/install.sh
 
-if is_ubuntu20; then
- sudo apt-get install libc6
- sudo apt-get update
- fi
 
 # Install the oc CLI
-archive_path=$(download_with_retry "https://mirror.openshift.com/pub/openshift-v4/clients/ocp/latest/openshift-client-linux.tar.gz")
+archive_path=$(download_with_retry "https://mirror.openshift.com/pub/openshift-v4/clients/ocp/latest-4.16/openshift-client-linux-4.16.0.tar.gz")
 tar xzf "$archive_path" -C "/usr/local/bin" oc
 
 invoke_tests "CLI.Tools" "OC CLI"
