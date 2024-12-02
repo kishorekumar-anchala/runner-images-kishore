934cf6f5abb20fe9b70972a301f5d154b132f264
Kishore Kumar Reddy Anchala <kishorekumar-anchala@github.com>
2024-12-02 16:51:37 +0530

    Intialzing the new directory and clearing the cache

--- images/windows/scripts/build/Install-PostgreSQL.ps1
+++ images/windows/scripts/build/Install-PostgreSQL.ps1
@@ -7,6 +7,22 @@
 $pgUser = "postgres"
 $pgPwd = "root"
 
+# Clean previous PostgreSQL installation
+Stop-Service -Name "postgresql*" -ErrorAction SilentlyContinue
+Get-Service -Name "postgresql*" -ErrorAction SilentlyContinue | Set-Service -StartupType Disabled
+
+$pathsToRemove = @(
+    "C:\Program Files\PostgreSQL",
+    "C:\PostgreSQL",
+    "C:\Users\$env:USERNAME\AppData\Local\Temp\postgresql_installer*",
+    "HKLM:\SOFTWARE\PostgreSQL\"
+)
+
+foreach ($path in $pathsToRemove) {
+    Remove-Item -Recurse -Force $path -ErrorAction SilentlyContinue
+}
+
+
 # Prepare environment variable for validation
 [Environment]::SetEnvironmentVariable("PGUSER", $pgUser, "Machine")
 [Environment]::SetEnvironmentVariable("PGPASSWORD", $pgPwd, "Machine")
@@ -73,6 +89,18 @@ $pgPath = (Get-CimInstance Win32_Service -Filter "Name LIKE 'postgresql-%'").Pat
 $pgBin = Split-Path -Path $pgPath.split('"')[1]
 $pgRoot = Split-Path -Path $pgPath.split('"')[5]
 $pgData = Join-Path $pgRoot "data"
+
+# Initialize data directory if not already initialized
+
+if (-Not (Test-Path (Join-Path $pgData "postgresql.conf"))) {
+
+    Write-Host "Initializing PostgreSQL data directory..."
+
+    $initDbPath = Join-Path $pgBin "initdb.exe"
+
+    & $initDbPath -D $pgData -U $pgUser -W $pgPwd
+
+}
 
 # Validate PostgreSQL installation
 $pgReadyPath = Join-Path $pgBin "pg_isready.exe"
