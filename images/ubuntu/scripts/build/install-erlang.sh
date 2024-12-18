afd0613cbc1a90cf8c630c184d24381afe7c0659
Kishore Kumar Reddy Anchala <kishorekumar-anchala@github.com>
2024-12-18 17:43:03 +0530

    Update erlang download link on ubunut 20

--- images/ubuntu/scripts/build/install-erlang.sh
+++ images/ubuntu/scripts/build/install-erlang.sh
@@ -6,34 +6,36 @@
 
 # Source the helpers for use with the script
 source $HELPER_SCRIPTS/install.sh
-
-# Add missing dependencies
-
-apt-get update
-
-apt-get install -y wget gnupg apt-transport-https lsb-release
+source $HELPER_SCRIPTS/os.sh
 
 source_list=/etc/apt/sources.list.d/eslerlang.list
 source_key=/usr/share/keyrings/eslerlang.gpg
 
-echo "Downloading Erlang GPG key..."
+# If Ubuntu 20.04, download Erlang manually
+if is_ubuntu20; then
 
-wget -q -O /tmp/erlang_solutions.asc https://packages.erlang-solutions.com/ubuntu/erlang_solutions.asc
+  ERLANG_PACKAGE="erlang_24.3.4-1~focal_amd64.deb"  # Change this based on version
+  ERLANG_URL="https://packages.erlang-solutions.com/ubuntu/pool/esl-erlang/$ERLANG_PACKAGE"
 
+  # Download the package
+  wget -q $ERLANG_URL -O /tmp/$ERLANG_PACKAGE
 
+  # Install the package manually
+  dpkg -i /tmp/$ERLANG_PACKAGE
+  apt-get install -f -y
 
-echo "Converting GPG key..."
+  echo "Erlang installed successfully on Ubuntu 20.04"
+else
+  # If not Ubuntu 20.04, use the package manager as usual
+  # Default Repository URL for Erlang Solutions
+  REPO_URL="https://packages.erlang-solutions.com/ubuntu $(lsb_release -cs) contrib"
 
-gpg --dearmor /tmp/erlang_solutions.asc > $source_key
-
-
-
-echo "Adding repository..."
-
-echo "deb [signed-by=$source_key] https://packages.erlang-solutions.com/ubuntu $(lsb_release -cs) contrib" > $source_list
-apt-get update
-
-apt-get install --no-install-recommends esl-erlang
+  # Install Erlang
+  wget -q -O - https://packages.erlang-solutions.com/ubuntu/erlang_solutions.asc | gpg --dearmor > $source_key
+  echo "deb [signed-by=$source_key] $REPO_URL" > $source_list
+  apt-get update
+  apt-get install --no-install-recommends esl-erlang
+fi
 
 # Install rebar3
 rebar3_url=$(resolve_github_release_asset_url "erlang/rebar3" "endswith(\"rebar3\")" "latest")
