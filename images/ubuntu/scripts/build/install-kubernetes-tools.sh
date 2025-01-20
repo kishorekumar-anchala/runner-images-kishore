be3403325e6e0646c218ab7fc9d02a4d9336f949
Kishore Kumar Reddy Anchala <kishorekumar-anchala@github.com>
2025-01-20 15:24:17 +0530

    Pinning the version

--- images/ubuntu/scripts/build/install-kubernetes-tools.sh
+++ images/ubuntu/scripts/build/install-kubernetes-tools.sh
@@ -28,12 +28,12 @@ apt-get install kubectl
 rm -f /etc/apt/sources.list.d/kubernetes.list
 
 # Install Helm
-curl -fsSL https://raw.githubusercontent.com/helm/helm/master/scripts/get | bash
+curl -fsSL https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 | bash
 # Download minikube
-curl -fsSL -O https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+curl -fsSL -O https://storage.googleapis.com/minikube/releases/v1.34.0/minikube-linux-amd64
 
 # Supply chain security - minikube
-minikube_hash=$(get_checksum_from_github_release "kubernetes/minikube" "linux-amd64" "latest" "SHA256")
+minikube_hash=$(get_checksum_from_github_release "kubernetes/minikube" "linux-amd64" "1.34.0" "SHA256")
 use_checksum_comparison "minikube-linux-amd64" "${minikube_hash}"
 
 # Install minikube
