a02e14b88d71b64f7e91c7d70e50a48ff776e5b2
Kishore Kumar Reddy Anchala <kishorekumar-anchala@github.com>
2025-01-20 15:05:24 +0530

    update the script

--- images/ubuntu/scripts/build/install-kubernetes-tools.sh
+++ images/ubuntu/scripts/build/install-kubernetes-tools.sh
@@ -28,11 +28,7 @@ apt-get install kubectl
 rm -f /etc/apt/sources.list.d/kubernetes.list
 
 # Install Helm
-curl -fsSL https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 | bash
-ls -al /usr/local/bin
-chmod +x linux-amd64/helm
-mv linux-amd64/helm /usr/local/bin/helm
-
+curl -fsSL https://raw.githubusercontent.com/helm/helm/master/scripts/get | bash
 # Download minikube
 curl -fsSL -O https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
 
