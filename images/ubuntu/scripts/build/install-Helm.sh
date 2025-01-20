14646df021cb0ef3e6e80793669fb0dcc8e1e111
Kishore Kumar Reddy Anchala <kishorekumar-anchala@github.com>
2025-01-20 12:16:08 +0530

    Helm sheet update

--- images/ubuntu/scripts/build/install-Helm.sh
+++ images/ubuntu/scripts/build/install-Helm.sh
@@ -5,5 +5,6 @@
 ################################################################################
 
 curl -fsSL https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 | bash
+cp /tmp/helm/linux-amd64/helm /usr/local/bin/helm
 
 invoke_tests "Tools" "Helm"
