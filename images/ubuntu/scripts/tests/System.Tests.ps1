67ea36a54300e2ddd6dd9183fcdfcaf12976ee58
Kishore Kumar Reddy Anchala <kishorekumar-anchala@github.com>
2025-05-23 14:16:57 +0530

    fix u-22 failure

--- images/ubuntu/scripts/tests/System.Tests.ps1
+++ images/ubuntu/scripts/tests/System.Tests.ps1
@@ -10,6 +10,6 @@ Describe "Disk free space" -Skip:(-not [String]::IsNullOrEmpty($env:AGENT_NAME) 
 Describe "fwupd removed" {
     It "Is not present on box" {
         $systemctlOutput = & systemctl is-enabled fwupd-refresh.timer
-        $systemctlOutput | Should -Match "masked"
+        $systemctlOutput | Should -BeExactly "masked"
     }
 }
