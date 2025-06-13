843c9f79425b39d005c87b6b9e09d86c8ae9691a
Kishore Kumar Reddy Anchala <kishorekumar-anchala@github.com>
2025-06-13 17:17:14 +0530

    [windows] Include checksum manully

--- images/windows/scripts/build/Install-Miniconda.ps1
+++ images/windows/scripts/build/Install-Miniconda.ps1
@@ -8,7 +8,7 @@ $condaDestination = "C:\Miniconda"
 $installerName = "Miniconda3-latest-Windows-x86_64.exe"
 
 Install-Binary `
-    -Url "https://repo.anaconda.com/miniconda/${installerName}" `
+    -Url "https://repo.anaconda.com/miniconda/$installerName" `
     -InstallArgs @("/S", "/AddToPath=0", "/RegisterPython=0", "/D=$condaDestination") `
     -ExpectedSHA256Sum "ea482f35b4861f047dc525c1e8dc645cc761a5460a121caf726dec098c5804de"
 
