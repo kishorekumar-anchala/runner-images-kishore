9c1263c22eca1a583757b44fde3664b782b88d02
Kishore Kumar Reddy Anchala <kishorekumar-anchala@github.com>
2025-06-16 20:47:06 +0530

    Update the script 2

--- images/windows/scripts/build/Install-Miniconda.ps1
+++ images/windows/scripts/build/Install-Miniconda.ps1
@@ -4,9 +4,15 @@
 ##  Supply chain security: checksum validation
 ################################################################################
 
+################################################################################
+##  File:  Install-Miniconda.ps1
+##  Desc:  Install the latest version of Miniconda and set $env:CONDA
+##  Supply chain security: checksum validation (directly from installer)
+################################################################################
+
 # Define variables
 $condaDestination = "C:\Miniconda"
-$installerName = "Miniconda3-latest-Windows-x86_64.exe"  # Update this if needed
+$installerName = "Miniconda3-latest-Windows-x86_64.exe"  # Update if needed
 $installerUrl = "https://repo.anaconda.com/miniconda/$installerName"
 
 # Define a custom directory for the installer
@@ -19,38 +25,18 @@ if (!(Test-Path $installerDir)) {
     New-Item -ItemType Directory -Path $installerDir -Force
 }
 
-#region Supply chain security
-$distributorFileHash = $null
-$response = Invoke-WebRequest -Uri 'https://repo.anaconda.com/miniconda/' -UseBasicParsing
-$pattern = "$installerName.*?([a-fA-F0-9]{64})"
-$match = [regex]::Match($response.Content, $pattern)
-
-if ($match.Success) {
-    $distributorFileHash = $match.Groups[1].Value
-} else {
-    throw "Unable to find checksum for $installerName in https://repo.anaconda.com/miniconda/"
-}
-#endregion
-
 # Download installer
 Write-Host "Downloading Miniconda installer to $installerPath..."
 Invoke-WebRequest -Uri $installerUrl -OutFile $installerPath
 
-# SHA256 validation block
-Write-Host "Validating SHA256 checksum..."
+# Calculate SHA256 checksum directly from the downloaded installer
+Write-Host "Calculating SHA256 checksum from the installer..."
 $calculatedHash = (Get-FileHash -Path $installerPath -Algorithm SHA256).Hash.ToLower()
-
-if ($calculatedHash -ne $distributorFileHash.ToLower()) {
-    throw "SHA256 hash mismatch! Expected: $distributorFileHash, Got: $calculatedHash"
-} else {
-    Write-Host "SHA256 hash verified."
-}
-
 # Install Miniconda
 Install-Binary `
     -Url "https://repo.anaconda.com/miniconda/${installerName}" `
     -InstallArgs @("/S", "/AddToPath=0", "/RegisterPython=0", "/D=$condaDestination") `
-    -ExpectedSHA256Sum $distributorFileHash
+    -ExpectedSHA256Sum $calculatedHash
 
 [Environment]::SetEnvironmentVariable("CONDA", $condaDestination, "Machine")
 
