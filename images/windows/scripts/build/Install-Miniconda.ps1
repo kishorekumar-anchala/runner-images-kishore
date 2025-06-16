76bd7d5e298916fef35642194ce569ce7ee7a16f
kishorekumar-anchala <168699259+kishorekumar-anchala@users.noreply.github.com>
2025-06-16 23:07:06 +0530

    Update Install-Miniconda.ps1

--- images/windows/scripts/build/Install-Miniconda.ps1
+++ images/windows/scripts/build/Install-Miniconda.ps1
@@ -4,7 +4,7 @@
 ##  Supply chain security: checksum validation
 ################################################################################
 
-$condaDestination = "C:\Miniconda"
+#$condaDestination = "C:\Miniconda"
 $installerName = "Miniconda3-latest-Windows-x86_64.exe"
 
 #region Supply chain security
@@ -24,7 +24,7 @@ if ($null -eq $distributorFileHash) {
 
 Install-Binary `
     -Url "https://repo.anaconda.com/miniconda/${installerName}" `
-    -InstallArgs @("/S", "/AddToPath=0", "/RegisterPython=0", "/D=$condaDestination") `
+    -InstallArgs @("/S", "InstallationType=JustMe","/AddToPath=0", "/RegisterPython=0", "/D=%UserProfile%\Miniconda3") `
     -ExpectedSHA256Sum $distributorFileHash
 
 [Environment]::SetEnvironmentVariable("CONDA", $condaDestination, "Machine")
