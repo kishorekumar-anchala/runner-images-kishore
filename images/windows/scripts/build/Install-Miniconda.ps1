a1ad5e10d7d332336932c72b913f0bcbf3ba74ba
Kishore Kumar Reddy Anchala <kishorekumar-anchala@github.com>
2025-06-18 18:40:21 +0530

    rollback the minconda script

--- images/windows/scripts/build/Install-Miniconda.ps1
+++ images/windows/scripts/build/Install-Miniconda.ps1
@@ -7,9 +7,25 @@
 $condaDestination = "C:\Miniconda"
 $installerName = "Miniconda3-latest-Windows-x86_64.exe"
 
+#region Supply chain security
+$distributorFileHash = $null
+$checksums = (ConvertFrom-HTML -Uri 'https://repo.anaconda.com/miniconda/').SelectNodes('//html/body/table/tr')
+
+foreach ($node in $checksums) {
+    if ($node.ChildNodes[1].InnerText -eq $installerName) {
+        $distributorFileHash = $node.ChildNodes[7].InnerText
+    }
+}
+
+if ($null -eq $distributorFileHash) {
+    throw "Unable to find checksum for $installerName in https://repo.anaconda.com/miniconda/"
+}
+#endregion
+
 Install-Binary `
     -Url "https://repo.anaconda.com/miniconda/${installerName}" `
-    -InstallArgs @("/S", "/AddToPath=0", "/RegisterPython=0", "/D=$condaDestination")
+    -InstallArgs @("/S", "/AddToPath=0", "/RegisterPython=0", "/D=$condaDestination") `
+    -ExpectedSHA256Sum $distributorFileHash
 
 [Environment]::SetEnvironmentVariable("CONDA", $condaDestination, "Machine")
 
