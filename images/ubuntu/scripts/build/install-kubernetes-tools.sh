b347bfc195187e7028f8244a1dcffcae4e97373e
Kishore Kumar Reddy Anchala <kishorekumar-anchala@github.com>
2025-06-26 16:26:35 +0530

    [Ubuntu] create a new key directory

--- images/ubuntu/scripts/build/install-kubernetes-tools.sh
+++ images/ubuntu/scripts/build/install-kubernetes-tools.sh
@@ -22,6 +22,8 @@ install "${kind_binary_path}" /usr/local/bin/kind
 ## Install kubectl
 kubectl_minor_version=$(curl -fsSL "https://dl.k8s.io/release/stable.txt" | cut -d'.' -f1,2 )
 kubectl_apt_version="v${kubectl_minor_version}"
+
+sudo mkdir -p -m 755 /etc/apt/keyrings
 curl -fsSL https://pkgs.k8s.io/core:/stable:/${kubectl_apt_version}/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
 echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/'${kubectl_apt_version}'/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
 apt-get update
